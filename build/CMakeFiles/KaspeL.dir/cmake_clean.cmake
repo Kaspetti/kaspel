a1b0fe48d8e81909d04cf18c81fb6a1cb9c0ff78
Kaspar Moberg <kaspeti123@gmail.com>
2023-05-16 02:31:56 +0200

    added cmake

---
+++ build/CMakeFiles/KaspeL.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KaspeL.dir/lexer/src/lexerfsm.cpp.o"
+  "CMakeFiles/KaspeL.dir/lexer/src/lexerfsm.cpp.o.d"
+  "CMakeFiles/KaspeL.dir/lexer/src/states/startstate.cpp.o"
+  "CMakeFiles/KaspeL.dir/lexer/src/states/startstate.cpp.o.d"
+  "CMakeFiles/KaspeL.dir/main.cpp.o"
+  "CMakeFiles/KaspeL.dir/main.cpp.o.d"
+  "KaspeL"
+  "KaspeL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KaspeL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/KaspeL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for KaspeL.
+# This may be replaced when dependencies are built.
